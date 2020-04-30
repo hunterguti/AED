8d48d21e7d176565bb20e62532a54f4840a7f344
hunterguti <manuelgutierrez012012@gmail.com>
2020-04-30 12:33:23 -0300

    modificacion

--- 01-Adicion/adicion.cpp
+++ 01-Adicion/adicion.cpp
@@ -5,11 +5,13 @@ using namespace std;
 int main(){
 
 int a;
+std::cout << "ingrese el primer numero: ";
 std::cin >> a;
 int b;
+std::cout << "ingrese el segundo numero: ";
 std::cin >> b;
 
-std::cout << a + b;
+std::cout <<"El resultado de la suma es " << a + b;
 
 
 }
